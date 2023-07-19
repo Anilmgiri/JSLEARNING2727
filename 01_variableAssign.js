56e8ac4ccbe53284fe136aba8fb193e654210cd9
Anilmgiri <118908091+Anilmgiri@users.noreply.github.com>
2023-07-19 21:31:48 +0530

    varible declaration and initilzation
    varible declaration and initilization

---
+++ 01_variableAssign.js
@@ -0,0 +1,35 @@
+var myFullName;
+console.log("type of variable myfullName is = ",myFullName);
+var myfirstName;
+firstName="Ananda"
+var lastName;
+lastName="mendhegiri"
+console.log("first name and last name is = Ananda mendhegiri",);
+var myFullName;
+myFullName="";
+console.log("my fullName is = Ananda chandrakant mendhegiri",myFullName);
+var myName;
+myName = "";
+console.log(" My Love : I LOVE ONLY JS",myName);
+var firstName;
+firstName = "";
+console.log("the first Name is = Ananda",firstName);
+var lastName;
+lastName = "";
+console.log("The last name is = mendhegiri",lastName);
+var age;
+age = "";
+console.log("The age is = 31",age);
+var address;
+address = "";
+console.log("The address is = dutta colony kaneri",address);
+var picode;
+pincode = 416234;
+console.log("pin code is = ",pincode);
+var gender;
+gender = "male";
+console.log("Gender is = ",gender);
+var isMarried;
+isMarried = false;
+console.log( "Are you married = ",isMarried);
+
---
+++ 01_vriable.js
@@ -0,0 +1,14 @@
+
+var myName;//variable declration
+myName = "ananda"; //varible assinment or Initializatin
+
+console.log(myName);
+
+var city ="kolhapur";
+console.log(city);
+ 
+var pin; //variaable declaraation
+console.log(pin);
+
+var pin ="416234";
+console.log(pin);
