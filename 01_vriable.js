325ac5c79170716aa004e0c422c3482cb26973df
Anilmgiri <118908091+Anilmgiri@users.noreply.github.com>
2023-07-20 10:29:53 +0530

    swaping in two num and three num

--- 01_vriable.js
+++ 01_vriable.js
@@ -12,3 +12,5 @@ console.log(pin);
 
 var pin ="416234";
 console.log(pin);
+var salary=null;
+console.log(typeof salary);
---
+++ 03_swapVarieble.js
@@ -0,0 +1,27 @@
+
+console.log("################### Assignment No-02 ###############");
+
+
+var name1 = "sweety";
+ var name2 = "cutie";
+console.log("Before Swaping is =  ", "name1-" ,name1, "name2-" ,name2);
+
+var  temp = name1;
+name1 = name2;
+name2 =temp;
+console.log("After swaping is =",  "name1-" ,name1, "name2-", name2);
+
+
+console.log("*************** Num swap*************");
+
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("Before swping is=", "num1 -",num1,"num2 -",num2,"num3 -",num3);
+var temp = num1;
+num1=num2;
+num2=temp;
+var temp= num2;
+num2=num3;
+num3=temp;
+console.log("After swaping is =" ,"num1 -" ,num1, "num2 -" ,num2, "num3 -", num3);
