554370b04349cefc63bcd723d32e5f133972b8a1
Anilmgiri <118908091+Anilmgiri@users.noreply.github.com>
2023-07-27 19:05:45 +0530

    commit the changes and
    commit the changes and

---
+++ 02_naN.js
@@ -0,0 +1,18 @@
+// NaN- Not a number
+var num1=0;
+var num2=0;
+var result=num1/num2;
+console.log(result);
+
+var num3="100";
+console.log(`Type of num3 is: ${typeOfnum3}`);
+var result= +num3;
+console.log(`type of result is :${result}` );
+console.log(`type of result is : ${typeOfresult}`);
+console.log("*****************************************");
+
+  var num4="GK";
+var resultNum4= +num4;
+ /*
+ //var resultNum =NaN
+  */
