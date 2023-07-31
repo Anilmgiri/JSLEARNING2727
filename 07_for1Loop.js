55bd0a62aa3aef979fb7aa54c5686df3dba66191
Anilmgiri <118908091+Anilmgiri@users.noreply.github.com>
2023-07-31 23:40:31 +0530

    commit & pushed
    commit & pushed

---
+++ 07_for1Loop.js
@@ -0,0 +1,47 @@
+
+  console.log("++++++++++++++++++++ number print 5 to 15 ++++++++++++++++++++");
+  var result =" ";
+   for (let index =5; index <15; index++) {
+    result=result.concat(index).concat(" ");
+    
+   }
+   console.log(result);
+   console.log("**************** resvers order 50 to 40 print *****************************");
+   var result =" ";
+   for (let index = 50; index >= 40; index--) {
+    result=result.concat(index).concat(" ");
+   }
+   console.log(result);
+   console.log("******************** first 15 odd number print  **************************");
+   var result =" ";
+   for (let index = 1; index <30; index=index+2) {
+    result=result.concat(index).concat(" ");
+    
+   }
+   console.log(result);
+   console.log("********************* first 10 even number print  *************************");
+   var result =" ";
+   for (let index = 0; index < 20; index=index+2) {
+    result=result.concat(index).concat(" ");
+   }
+   console.log(result);
+   console.log("***************** 5 table print  ****************************");
+   var result =" ";
+   for (let index = 5; index < 51;  index=index+5) {
+    result=result.concat(index).concat(" ");
+   }
+   console.log(result);
+    console.log("***************** 10 table print ***************************");
+    var result =" ";
+    for (let index = 10; index < 101; index=index+10) {
+      result=result.concat(index).concat(" ");
+    }
+    console.log(result);
+      console.log("*************** 10 reverse order **********************");
+      var result =" ";
+      for (let index =100; index >=10; index=index-10) {
+        result=result.concat(index).concat(" ");
+      }
+      console.log(result);
+        
+      
---
+++ 07_whileLoop.js
@@ -0,0 +1,30 @@
+console.log("*************** 5 to 15 number by increament  1 *******************");
+var result=" ";
+ var i=5 ,n=15;
+ while (i<=15) {
+    i++;
+    result=result.concat(i).concat(" ");
+    
+ }
+ console.log(result);
+  
+  console.log("****************  50 to 40  reverse print ********************");
+    var result=" ";
+    var i=50,n=40;
+    while (i>=40) {
+        i--;
+        result=result.concat(i).concat(" ");
+    
+    }
+    console.log(result);
+    console.log("******************* first 15 odd number print ******************");
+     var result=" ";
+     var i=1,n=21;
+     while (i>=21) {
+        i+1;
+        result=result.concat(i).concat(" ");
+     }
+     console.log(result);
+     console.log("******************* first 10 even number print ******************");
+        
+    
